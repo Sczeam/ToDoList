723024886fe3fe8873c46c240fe9d9dbddf34ce9
Sczeam <zawhtikeaung19@gmail.com>
2024-10-05 13:01:12 +0100

    box correctly showed

--- ToDoWebapp/src/components/ToDoList.tsx
+++ ToDoWebapp/src/components/ToDoList.tsx
@@ -15,7 +15,7 @@ export const ToDoList: React.FC = () => {
             className="ml-1 text-center w-full h-full font-poppins font-extrabold"
           />
         </div>
-        <div className="flex flex-inline justify-center items-center h-[30px] col-span-4 sm:col-span-1 w-full">
+        <div className="relative flex flex-inline justify-center items-center h-[30px] col-span-4 sm:col-span-1 w-full">
           <span
             onClick={ToogleSetStatus}
             className="flex justify-center items-center font-poppins h-full w-full font-base text-[8px] sm:text-[10px] font-extrabold cursor-pointer"
@@ -37,7 +37,9 @@ export const ToDoList: React.FC = () => {
               d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
             />
           </svg>
-          <div className={`w-[100px] h-[200px] bg-teal-500`}></div>
+          {isSetStatusClick && (
+            <aside className="rounded-lg shadow absolute top-6 -right-5 w-[100px] h-[60px] bg-teal-500"></aside>
+          )}
         </div>
       </div>
       <div className="mt-1 h-[30px] w-full bg-pink-500"></div>
