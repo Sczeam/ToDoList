3e8ede4dd45717dbaa1176a950a13542c0e6f318
Sczeam <zawhtikeaung19@gmail.com>
2024-10-08 22:13:37 +0100

    todolist finished

--- ToDoWebapp/src/components/ToDoList.tsx
+++ ToDoWebapp/src/components/ToDoList.tsx
@@ -399,18 +399,56 @@ const TaskItem: React.FC<TaskItemProps> = ({ task, type }) => {
   const style = {
     transform: CSS.Translate.toString(transform),
     opacity: isDragging ? 0.5 : 1,
+    cursor: isDragging ? "grabbing" : "grab", // Fixed cursor declaration
+    touchAction: "none",
   };
 
+  const taskFinished = () => {
+    console.log("clicked");
+  };
   return (
     <li
       ref={setNodeRef}
       style={style}
       {...listeners}
       {...attributes}
-      className="min:h-[30px] w-full grid grid-cols-12 divide-x-2 p-1"
+      className="cursor-pointer sm:cursor-grab h-[50px] sm:min:h-[30px] w-full grid grid-cols-12 divide-x-2 p-1"
     >
-      <span className="col-span-12 sm:col-span-3 flex items-center">
+      <span
+        onClick={taskFinished}
+        className="col-span-12 sm:col-span-3 flex items-center"
+      >
         {/* task icon */}
+        {!task.isDone ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        )}
+
         {task.taskName}
       </span>
       <span className="hidden sm:flex col-span-3"></span>
